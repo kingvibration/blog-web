f6963ad748079b816fd7eeeeb26b619953205acc
kingvibration <wangzhen@yunjiacloud.com>
2021-01-13 14:52:23 +0800

    fix init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    title: 'Wang\'s blog',
+    title: 'kingvibration\'s blog',
     description: '我的个人网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
         ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
-        lineNumbers: false // 代码块显示行号
+        lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
         nav:[ // 导航栏配置
